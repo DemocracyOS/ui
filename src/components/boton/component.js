4a9ada7d8462f001898971cee19732e613fb79ae
luarar <laurarh.971@gmail.com>
2018-08-21 12:23:23 -0300

    agrega boton

---
+++ src/components/boton/component.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Button = styled.button`
+  display: inline-block;
+  color:#5e99bc;
+  font-size: 1em;
+  font-weight:600;
+  margin: 1em;
+  padding: 0.5em 2em;
+  border: 2px solid #A5CEE7;
+  border-radius: 20px;
+`;
+
+
+export default function Boton() {
+return (
+    <Button>
+        Hola
+    </Button>
+  )}
