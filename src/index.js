3951d488e0a936e6b442c3616591d8a1e46a6507
florlafuente <lafuente.florencia@gmail.com>
2018-08-23 16:37:19 -0300

    Add suggestion component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
 import React from 'react'
 import Button from './components/button/component'
+import Suggestion from './components/suggestion/component'
 
-export { Button }
+export { 
+  Button,
+  Suggestion
+}
