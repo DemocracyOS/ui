79daf9675ebfa3c4e64eb0e0f92b6ae2a38778cd
florlafuente <lafuente.florencia@gmail.com>
2018-08-27 14:58:46 -0300

    Add react-router

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Router, Route } from 'react-router';
+import createMemoryHistory from 'react-router/lib/createMemoryHistory';
 import { storiesOf } from '@storybook/react'
 import { addDecorator } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
@@ -9,6 +11,11 @@ import NavBarTitle from '../src/elements/navbar-title/component'
 addDecorator((story, context) => withInfo('common info')(story)(context))
 
 storiesOf('elements/NavBarTitle', module)
+  .addDecorator((story) => (
+    <Router history={createMemoryHistory('/')}>
+      <Route path='/' component={() => story()} />
+    </Router>
+  ))
   .add('Navbar Title', () => (
     <NavBarTitle>Default title</NavBarTitle>
   ))
