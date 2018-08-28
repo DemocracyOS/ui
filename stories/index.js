8c44ac6708c2d9825cfea0b0c253ef644f8195fc
luarar <laurarh.971@gmail.com>
2018-08-28 16:36:54 -0300

    add h3

--- stories/index.js
+++ stories/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Router } from 'react-router';
+import { Router } from 'react-router'
 import createMemoryHistory from 'history/createMemoryHistory'
 import { storiesOf } from '@storybook/react'
 import { addDecorator } from '@storybook/react'
@@ -8,16 +8,13 @@ import { withInfo } from '@storybook/addon-info'
 import { withNotes } from '@storybook/addon-notes'
 import '../src/index.css'
 import NavBarTitle from '../src/elements/navbar-title/component'
-<<<<<<< 8b2c09f86d5600e19f736ea164a5a88962ae12ef
 import UserLink from '../src/elements/user-link/component'
 import UserBar from '../src/components/user-bar/component'
 import LinkbarLink from '../src/elements/linkbar-link/component'
 import Linkbar from '../src/components/linkbar/component'
-import HeroTitle from '../src/elements/hero-title/component'
-import  { HeroSubtitle, HeroButton, HeroImg } from '../src/index.js'
-=======
+import  { HeroTitle, HeroSubtitle, HeroButton, HeroImg } from '../src/index.js'
 import TitleH2 from '../src/elements/title-h2/component'
->>>>>>> add h2 element
+import SubtitleH3 from '../src/elements/subtitle-h3/component'
 
 addDecorator((story, context) => withInfo('common info')(story)(context))
 addDecorator((story) => <Router history={history}>{story()}</Router>)
@@ -82,8 +79,14 @@ storiesOf('elements/HeroButton', module)
 storiesOf('elements/HeroImg', module)
   .add('Hero Img', () => (
     <HeroImg src='' />
+  ))
+
 storiesOf('elements/Title-h2', module)
   .add('H2 - Title', () => (
     <TitleH2>Proyectos en debate</TitleH2>
+  ))
 
+storiesOf('elements/subtitle-h3', module)
+  .add('H3 - Subtitle', () => (
+    <SubtitleH3>Estos son los proyectos</SubtitleH3>
   ))
