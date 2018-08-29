509f10b45754133135d080dc829d49f5a9a1d118
florlafuente <lafuente.florencia@gmail.com>
2018-08-29 14:41:33 -0300

    Bump to version 0.1.4

--- dist/elements/navbar-title/component.js
+++ dist/elements/navbar-title/component.js
@@ -4,7 +4,7 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-var _templateObject = _taggedTemplateLiteral(['\n  font-family: var(--bold);\n  font-size: 3rem;\n  color: #192a34;\n  text-decoration: none;\n'], ['\n  font-family: var(--bold);\n  font-size: 3rem;\n  color: #192a34;\n  text-decoration: none;\n']);
+var _templateObject = _taggedTemplateLiteral(['\n  > a {\n    font-family: var(--bold);\n    font-size: 3rem;\n    color: #192a34;\n    text-decoration: none;\n  }\n'], ['\n  > a {\n    font-family: var(--bold);\n    font-size: 3rem;\n    color: #192a34;\n    text-decoration: none;\n  }\n']);
 
 var _react = require('react');
 
@@ -14,8 +14,6 @@ var _propTypes = require('prop-types');
 
 var _propTypes2 = _interopRequireDefault(_propTypes);
 
-var _reactRouterDom = require('react-router-dom');
-
 var _styledComponents = require('styled-components');
 
 var _styledComponents2 = _interopRequireDefault(_styledComponents);
@@ -24,22 +22,19 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 function _taggedTemplateLiteral(strings, raw) { return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 
-var Title = _styledComponents2.default.a(_templateObject);
+var Title = _styledComponents2.default.span(_templateObject);
 
-var NavBarTitle = function NavBarTitle(props) {
+var NavBarTitle = function NavBarTitle(_ref) {
+  var children = _ref.children;
   return _react2.default.createElement(
-    _reactRouterDom.Link,
-    { to: '/' },
-    _react2.default.createElement(
-      Title,
-      null,
-      props.children
-    )
+    Title,
+    null,
+    children
   );
 };
 
 NavBarTitle.propTypes = {
-  children: _propTypes2.default.string.isRequired
+  children: _propTypes2.default.node.isRequired
 };
 
 exports.default = NavBarTitle;
