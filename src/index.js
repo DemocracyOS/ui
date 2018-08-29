315ed520859098801e249dcb8b1303d9910086c4
florlafuente <lafuente.florencia@gmail.com>
2018-08-29 11:55:54 -0300

    Refactor src/index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import './index.css'
-// import Button from './components/button/component'
+// Import elements
 import NavBarTitle from './elements/navbar-title/component'
 import UserLink from './elements/user-link/component'
-import UserBar from './components/user-bar/component'
 import HeroTitle from './elements/hero-title/component'
 import HeroSubtitle from './elements/hero-subtitle/component'
 import HeroButton from './elements/hero-button/component'
 import HeroImg from './elements/hero-img/component'
+import LinkbarLink from './elements/linkbar-link/component'
+import SubtitleH3 from './elements/subtitle-h3/component'
+import TitleH2 from './elements/title-h2/component'
+// Import components
+import LinkBar from './components/linkbar/component'
+import UserBar from './components/user-bar/component'
 
+// Export all components
 export {
   NavBarTitle,
   UserLink,
@@ -16,5 +22,9 @@ export {
   HeroTitle,
   HeroSubtitle,
   HeroButton,
-  HeroImg
+  HeroImg,
+  LinkbarLink,
+  SubtitleH3,
+  TitleH2,
+  LinkBar
 }
